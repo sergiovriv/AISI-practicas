3101df760687a906a428c22b1fec12bc9ff368a1
Sergio Vila <78736098+sergiovriv@users.noreply.github.com>
2025-02-12 18:54:28 +0100

    delete

--- practica0-main/html/get-info.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $id = exec('uname -nrs');
-  $ver = exec('cat /etc/debian_version');
-  $cpu = exec('cat /proc/cpuinfo | grep "model name" | head -1 | cut -d ":" -f 2');
-  echo "<p>Debian $ver $id</p>";
-  echo "<p>$cpu</p>\n";
-  include 'db-get-data.php' ;
-?>
